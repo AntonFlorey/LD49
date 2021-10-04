336b5adb5cdf9f3ec7f80bb48f46de1a3f3b7c58
anton.florey <anton.florey@googlemail.com>
2021-10-04 23:34:07 +0200

    i hope its a fix

--- Assets/Scripts/TileComponent.cs
+++ Assets/Scripts/TileComponent.cs
@@ -38,7 +38,7 @@ public class TileComponent : MonoBehaviour
         if (newType == null)
         {
             this.IsBreaking = true;
-			this.breakingAnimation.enabled = true;
+			this.breakingAnimation.enabled = true;	
 			this.breakingAnimation.Play("BlockDestroy", 0, 0.0f);
         }
         else
